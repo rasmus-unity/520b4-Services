f5a1578cfa9d935a337d01a07e464cd8c1f155f4
Rasmus Møller Selsmark <rasmus@unity3d.com>
2015-07-23 14:46:59 +0300

    Enabled Ads

---
+++ Assets/Main.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class Main : MonoBehaviour
+{
+	public void ShowAd()
+	{
+		if (!Advertisement.IsReady())
+		{
+			Debug.Log("=== Ads not ready ===");
+			return;
+		}
+
+		Advertisement.Show();
+	}
+}
